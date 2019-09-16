6034aa09a679a368934ef6461142be4100aae722
robert-barcelona <robertanthony.bcn@gmail.com>
2019-09-16 22:17:46 +0200

    add back email and string checking

--- src/logic/index.js
+++ src/logic/index.js
@@ -2,15 +2,21 @@ import validateEmail from "../utils/validate-email";
 import User from "../models/User";
 import logger from "../utils/logger";
 
-export const logic = {
 
+
+
+export const logic = {
+  _validateStringField(value) {
+    return (typeof value === 'string' && value.length)
+  },
 
   async authenticate(email, password) {
     logger.debug(
       `authenticate,  CONTEXT: "logic/index.js", EMAIL:${email}, PASSWORD:${password}`
     );
-
-    const user = await User.findOne({ email }).exec();
+    if (!validateEmail(email)) throw new LogicError(`Email ${email} is not valid`)
+    if (!this._validateStringField(password)) throw new LogicError(`Password ${email} is not a valid string`)
+    const user = await User.findOne({email}).exec();
     if (!user) throw new LogicError(`user with email ${email} does not exist`);
     if (user.password !== password) throw new LogicError(`wrong password`);
     return user;
@@ -18,8 +24,9 @@ export const logic = {
 
   async getUser(email) {
     logger.debug(`getUser,  CONTEXT: "logic/index.js", EMAIL:${email}`);
+    if (!validateEmail(email)) throw new LogicError(`Email ${email} is not valid`)
 
-    const user = await User.findOne({ email }).exec();
+    const user = await User.findOne({email}).exec();
     if (!user) throw new LogicError(`user with email ${email} does not exist`);
     return user;
   }
